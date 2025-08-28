71f35b28bc0d18605f47c6e2d60cfa5bbfb375a1
ali-ahmadi-official <alihmad.1384@gmail.com>
2025-08-28 20:43:38 +0330

    site updated

--- config/settings.py
+++ config/settings.py
@@ -120,10 +120,8 @@ import os
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
